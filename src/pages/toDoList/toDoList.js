e9b16cc6f0eeeee9b291a92abeb2962d24df0082
artemPradko24 <artempradko24@gmail.com>
2021-10-12 16:53:12 +0300

    const changeValue

--- src/pages/toDoList/toDoList.js
+++ src/pages/toDoList/toDoList.js
@@ -1,31 +1,55 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+// import _ from 'lodash';
 
 import './toDoList.scss';
 
-function toDo() {
-  const addList = ({ todo }) => {
-    // eslint-disable-next-line lodash/prefer-lodash-method
-    // eslint-disable-next-line array-callback-return
-    const addOne = List.map((item) => {
-      const { id, ...deletes } = item;
+function ToDo() {
+  const [itemList, setNewItem] = useState([]);
+  const [newValue, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    console.info('e.target ---', e.target.value);
+
+    const { value } = e.target;
+
+    setValue(value);
+  }, []);
+
+  const addNewItem = useCallback(() => {
+    setNewItem((prevState) => {
+      const newItemState = [...prevState, newValue];
+
+      return newItemState;
     });
 
-    return (
-      <li key={id} className="list-group-item">
-        {...itemProps} onDeleted={() => onDeleted(id)}
-      </li>
-    );
-  };
+    setValue('');
+  }, [newValue]);
 
-  const list = ['Get Up'];
+  const changeValue = useCallBack((change) => {
+    setNewItem((prevState) => {
+      const newItemState = [...prevState, itemList[change]];
+
+      return newItemState;
+    });
+  });
 
   return (
     <div>
-      <input onClick={list.push()} type="text" placeholder="add" />
-      <li>{list}</li>
+      <input
+        onChange={onChange}
+        type="text"
+        placeholder="Add another one"
+        value={newValue}
+      />
+      <button onClick={addNewItem}>Add</button>
+      <ul>
+        {itemList.map((item) => (
+          <li key={itemList.indexOf(item)}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default toDo;
+export default ToDo;
